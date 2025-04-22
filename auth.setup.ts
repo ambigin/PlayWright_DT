4ea09d52cafe7751f85e75247b17a5d7c1f6cc46
SujanManandhar <xujan1966@gmail.com>
2025-04-22 17:44:23 +0545

    Updated auth file

--- auth.setup.ts
+++ auth.setup.ts
@@ -5,10 +5,10 @@ async function globalSetup(config: FullConfig) {
     const page = await browser.newPage();
     await page.goto('https://signin.coxautoinc.com/?solutionID=DTCOM_qa&clientId=c98d164b51aa4e0587bb0d208766fe23');
     await page.getByRole('textbox', { name: 'Username' }).click();
-    await page.getByRole('textbox', { name: 'Username' }).fill('smanan');
+    await page.getByRole('textbox', { name: 'Username' }).fill('');
     await page.getByRole('button', { name: 'Next' }).click();
     await page.getByRole('textbox', { name: 'Password' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('Toast@123');
+    await page.getByRole('textbox', { name: 'Password' }).fill('');
     await page.getByRole('button', { name: 'Sign in' }).click();
     await page.pause();
     await page.getByRole('link', { name: 'Switch Dealership' }).click();
